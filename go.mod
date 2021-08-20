8a608e71b9f954a4215ca6a13e725f4fa24f5a25
praveen4g0 <pthangad@redhat.com>
2021-08-21 00:49:55 +0530

    Added main.go

--- go.mod
+++ go.mod
@@ -5,25 +5,25 @@ go 1.15
 require (
 	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.0 // indirect
-	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi v1.5.4
 	github.com/go-openapi/analysis v0.20.1 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/runtime v0.19.30 // indirect
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/go-swagger/go-swagger v0.27.0 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/viper v1.8.1 // indirect
-	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125 // indirect
-	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
-	go.mongodb.org/mongo-driver v1.7.1 // indirect
+	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	go.mongodb.org/mongo-driver v1.7.1
 	golang.org/x/mod v0.5.0 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
 	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.5 // indirect
-	gopkg.in/dealancer/validate.v2 v2.1.0 // indirect
+	gopkg.in/dealancer/validate.v2 v2.1.0
 )
--- main.go
+++ main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	h "github.com/praveen4g0/url-shortner/api"
+	lr "github.com/praveen4g0/url-shortner/repository/local"
+	mr "github.com/praveen4g0/url-shortner/repository/mongodb"
+	rr "github.com/praveen4g0/url-shortner/repository/redis"
+
+	"github.com/praveen4g0/url-shortner/shortner"
 )
 
 // repo <- service -> serializer  -> http
 
 func main() {
-	errs := make(chan error, 1)
+	repo := chooseRepo()
+	service := shortner.NewRedirectService(repo)
+	handler := h.NewHandler(service)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+
+	errs := make(chan error, 2)
+	go func() {
+		fmt.Println("Listening on port :", httpPort())
+		errs <- http.ListenAndServe(httpPort(), r)
+	}()
 
 	go func() {
 		c := make(chan os.Signal, 1)
@@ -20,4 +49,40 @@ func main() {
 
 	fmt.Printf("Terminated %s", <-errs)
 	os.Exit(0)
+}
+
+func httpPort() string {
+	port := "8000"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	return fmt.Sprintf(":%s", port)
+}
+
+func chooseRepo() shortner.RedirectRepository {
+	switch os.Getenv("DB") {
+	case "redis":
+		redisURL := os.Getenv("REDIS_URL")
+		repo, err := rr.NewRedisRepository(redisURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	case "mongo":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongodb := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+		repo, err := mr.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	default:
+		repo, err := lr.NewLocalRepository()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	}
+	return nil
 }
